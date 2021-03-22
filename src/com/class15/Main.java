017eac49347936dba4cb4db6af003787deb54c0d
darynazarenko <darynazarenko95@gmail.com>
2021-03-22 15:41:55 -0500

    new commit

--- src/com/class15/Main.java
+++ src/com/class15/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
 	String str1(String str){
 	    String str1="test";
-	     str1= str2.toUpperCase();
+	     return str1= str.toUpperCase();
 	  }
 
 
@@ -15,7 +15,7 @@ public class Main {
 
 	  public static void main(String[] args){
 	    Main obj=new Main();
-	    System.out.println(obj.str2("test"));
+	    System.out.println(obj.str1("test"));
 	    
 		
 	}}
